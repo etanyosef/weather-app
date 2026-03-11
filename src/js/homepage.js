bb1513d5f8f43f2f9dc308798d6c6bb539e4bb02
Ethan <ethancredo.ascc@outlook.com>
2026-03-11 17:01:42 +0800

    weatherData object and display weather function

--- src/js/homepage.js
+++ src/js/homepage.js
@@ -5,8 +5,31 @@ export async function getWeather(location) {
         const queryURL = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${APIKey}`;
         const response = await fetch(queryURL);
         const queryData = await response.json();
+
+        const weatherData = {
+            address: queryData.address,
+            currentConditions : {
+                conditions: queryData.currentConditions.conditions,
+                feelsLike: queryData.currentConditions.feelslike,
+                humidity: queryData.currentConditions.humidity,
+                sunrise: queryData.currentConditions.sunrise,
+                sunset: queryData.currentConditions.sunset,
+                temp: queryData.currentConditions.temp,
+                windDir: queryData.currentConditions.winddir,
+                windSpeed: queryData.currentConditions.windspeed,
+            },
+
+        };
+
         console.log(queryData);
+
+        displayWeather(weatherData);
     } catch(error) {
         console.log(error);
     }
+}
+
+function displayWeather(weatherData) {
+    console.log(weatherData);
+    // console.log(weatherData.address);
 }
