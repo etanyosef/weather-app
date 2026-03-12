55f972bced0de04cc7ead363b29a612cfff64632
Ethan <ethancredo.ascc@outlook.com>
2026-03-12 10:36:31 +0800

    create getWeatherData function and return weatherData object

--- src/js/homepage.js
+++ src/js/homepage.js
@@ -6,30 +6,41 @@ export async function getWeather(location) {
         const response = await fetch(queryURL);
         const queryData = await response.json();
 
-        const weatherData = {
-            address: queryData.address,
-            currentConditions : {
-                conditions: queryData.currentConditions.conditions,
-                feelsLike: queryData.currentConditions.feelslike,
-                humidity: queryData.currentConditions.humidity,
-                sunrise: queryData.currentConditions.sunrise,
-                sunset: queryData.currentConditions.sunset,
-                temp: queryData.currentConditions.temp,
-                windDir: queryData.currentConditions.winddir,
-                windSpeed: queryData.currentConditions.windspeed,
-            },
-
-        };
-
         console.log(queryData);
 
-        displayWeather(weatherData);
+        displayWeather(queryData);
     } catch(error) {
         console.log(error);
     }
 }
 
-function displayWeather(weatherData) {
+function getWeatherData(queryData) {
+    const weatherData = {
+        address: queryData.address,
+        currentConditions : {
+            conditions: queryData.currentConditions.conditions,
+            dew: queryData.currentConditions.dew,
+            feelsLike: queryData.currentConditions.feelslike,
+            humidity: queryData.currentConditions.humidity,
+            icon: queryData.currentConditions.icon,                
+            sunrise: queryData.currentConditions.sunrise,
+            sunset: queryData.currentConditions.sunset,
+            temp: queryData.currentConditions.temp,
+            uvIndex: queryData.currentConditions.uvindex,
+            visibility: queryData.currentConditions.visibility,
+            windDir: queryData.currentConditions.winddir,
+            windSpeed: queryData.currentConditions.windspeed,
+        },
+        description: queryData.description,
+        latitude: queryData.latitude,
+        longitude: queryData.longitude,
+    };
+
+    return weatherData;
+}
+
+function displayWeather(queryData) {
+    const weatherData = getWeatherData(queryData);
     console.log(weatherData);
     // console.log(weatherData.address);
 }
